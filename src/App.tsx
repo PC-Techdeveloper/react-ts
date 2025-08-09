0c9c47bfcf7bcacaa4bce4c48f428bc166c5bc7b
Jefferson <jefersonchavezdiaz.14@hotmail.com>
2025-08-09 11:02:03 -0500

    continue on lists

--- src/App.tsx
+++ src/App.tsx
@@ -5,16 +5,42 @@ import MyButton from './Components/MyButton';
 
 const App = () => {
   const user = {
-    username: 'Ignacio',
+    username: 'James',
     imageUrl: 'https://i.pravatar.cc/150?img=3',
     imageSize: 90,
+    isLoggedIn: true,
   };
 
   // Desestructuring de objetos
-  const { username, imageUrl, imageSize } = user;
+  const { username, imageUrl, imageSize, isLoggedIn } = user;
+
+  /* Renderizado Condicional: Renderizar contenido utilizando los operadores lógicos o ternarios */
+
+  const loggedIn = true;
+
+  // Primera Forma:
+  if (!loggedIn) {
+    return console.log('Por favor, Inicia sesión.❌');
+  }
+
+  // Segunda Forma: Operador ternario y short-circuit operator (&&)
+  if (!isLoggedIn) {
+    return (
+      <>
+        <h2>Este usuario no existe</h2>
+        <p>Por favor, inicia sesión...</p>
+      </>
+    );
+  }
 
   return (
     <>
+      {/* Operador ternario */}
+      <h1>
+        {username ? username : <button>Agregar nombre de usuario</button>}
+      </h1>
+      {/* short-circuit operator */}
+      {username && <h2>Hola!: {username}</h2>}
       <div>
         <h1>Hola: {username}</h1>
         <img
---
+++ src/Components/SecondApp.tsx
@@ -0,0 +1,11 @@
+const SecondApp = () => {
+  return (
+    <>
+      <p>
+        Este es un componente para listas en <strong>React</strong>
+      </p>
+    </>
+  );
+};
+
+export default SecondApp;
