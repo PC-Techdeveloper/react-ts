4f1ccde12c2544313593fc26f173825574e26dd9
Jefferson <jefersonchavezdiaz.14@hotmail.com>
2025-08-10 16:39:47 -0500

    learning about promises

--- src/Components/AsyncAwait.tsx
+++ src/Components/AsyncAwait.tsx
@@ -1,7 +1,45 @@
+import { useEffect, useState } from 'react';
+
 const AsyncAwait = () => {
+  interface Todo {
+    userId: number;
+    id: number;
+    title: string;
+    completed: boolean;
+  }
+  const [data, setData] = useState<null | Todo>(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState<null | string>(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const res = await fetch('https://jsonplaceholder.typicode.com/todos/1');
+        if (!res.ok) throw new Error('Error al consumir la api');
+        const result = (await res.json()) as Todo;
+
+        setData(result);
+      } catch (error: unknown) {
+        if (error instanceof Error) {
+          setError(error.message);
+        } else {
+          setError('Error desconocido');
+        }
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>{error}</p>;
+
   return (
-    <div>
-      <h1>Llamadas a APIs con Async/Await</h1>
+    <div className="container mx-auto">
+      <h1>Fetch y useEffect</h1>
+      <pre>{JSON.stringify(data, null, 2)}</pre>
     </div>
   );
 };
